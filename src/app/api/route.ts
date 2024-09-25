dcd3a5d8c39821a9e265d2029dc7a426bf0da272
richeek45 <richeek45@gmail.com>
2024-09-25 22:05:10 +0530

    shadcn

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,16 @@
 import { NextRequest, NextResponse } from "next/server"; 
 import '../../../envConfig';
 import { YoutubeTranscript } from 'youtube-transcript';
+// import transcriptJson from "../../utils/transcript.json";
 
 export async function GET(request: NextRequest) { 
   const videoId = request.nextUrl.searchParams.get('videoId');
+  console.log(videoId);
   if (videoId) {
     const res = await YoutubeTranscript.fetchTranscript(videoId);
     return NextResponse.json({ data: res });
   }
 
+  // return NextResponse.json({data: transcriptJson });
   return NextResponse.json({data: '' });
 }
